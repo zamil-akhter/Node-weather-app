c9894b9d1c4d40a101384ffb71324dab986ca60d
Zamil Akhter <zamilakhter.nwl@gmail.com>
2024-09-03 22:49:46 +0530

    utility function created

--- server.js
+++ server.js
@@ -1,41 +1,32 @@
 const express = require("express");
 const axios = require("axios");
+const utils = require('./src/utils/utils');
 require("dotenv").config();
 
 const app = express();
 app.set("view engine", "ejs");
-// app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 7000;
 
 const init = async () => {
   try {
     app.get("/", (req, res) => {
-      try {
-        res.render("search-address");
-      } catch (error) {
-        throw error
-      }
+      res.render("search-address");
     });
 
     app.post("/weather", async (req, res) => {
       try {
-        let address= req.body.location;
+        let address = req.body.location;
         // console.log(".............>>>", address);
-        let mapUrl = `https://geocode.maps.co/search?q=${address}&api_key=${process.env.MAP_APIKEY}`;
-
-        const  mapApiResponse =await axios.get(mapUrl);
-        // console.log(mapApiResponse.data[0]);
+       
+        const { lat, lon } = await utils.getGeolocation(address);
         
-        let lat = mapApiResponse.data[0].lat;
-        let lon = mapApiResponse.data[0].lon;
-        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.APIKEY}`;
-        let response = await axios.get(url);
-        let responseData = response.data;
-        // console.log(responseData);
-        res.render("weather", responseData);
-        // return res.status(200).json(responseData);
+        const weatherData = await utils.getWeatherData(lat, lon);
+        // console.log(weatherData);
+
+        res.render("weather", weatherData);
       } catch (error) {
+        console.error("Error fetching weather data:", error.message);
         return res.status(500).json({ error: error.message });
       }
     });
---
+++ src/utils/utils.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+
+const getGeolocation = async (address) => {
+  const mapUrl = `https://geocode.maps.co/search?q=${address}&api_key=${process.env.MAP_APIKEY}`;
+  const response = await axios.get(mapUrl);
+  if (response.data.length === 0) {
+    throw new Error("Location not found");
+  }
+  return response.data[0];
+};
+
+const getWeatherData = async (lat, lon) => {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.APIKEY}`;
+  const response = await axios.get(weatherUrl);
+  return response.data;
+};
+
+module.exports = {
+  getGeolocation,
+  getWeatherData,
+};
